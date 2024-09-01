b1a050f06a42da7c473339ca7be90930e33929a6
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-09-01 21:42:17 +0530

    change: navbar

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NAVBAR_ITEMS = [
+  { title: "Home" },
+  { title: "About" },
+  { title: "Explore work" },
+  { title: "Contact Me" },
+];
+
+export function Navbar() {
+  return (
+    <nav className="fixed bottom-14 left-1/2 transform -translate-x-1/2 w-[80%]">
+      <div className="bg-white rounded-2xl shadow-md px-8 py-6">
+        <ul className="flex justify-around items-center">
+          {NAVBAR_ITEMS.map((navItem) => (
+            <li key={navItem.title}>
+              <a href="#" className="text-black text-2xl font-bold hover:text-gray-900 transition-colors">
+                {navItem.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
