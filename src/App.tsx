d271e108cb5a4d58b79ac99cfdd3d8d36dde8f07
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-09-01 21:52:06 +0530

    change: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Header } from "./components/ui/header";
 import { Navbar } from "./components/ui/navbar";
 
 function App() {
   return (
     <div className="h-screen w-full">
+      <Header />
       <Navbar />
     </div>
   );
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export function Header() {
-  return <div>header</div>;
-};
+  return (
+    <div className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full">
+      <section className="flex items-center justify-between px-8 py-4">
+        <div>
+          <h1 className="text-3xl font-bold text-black">R. Bharat</h1>
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <button className="bg-[#1B6C44] rounded-2xl px-4 py-3 text-white font-bold">
+            Hire me
+          </button>
+          <button>Dark light mode</button>
+          <button>Music</button>
+        </div>
+      </section>
+    </div>
+  );
+}
