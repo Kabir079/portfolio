cf21160f5e5c01e54eb86a51e423b4d6e23a3f00
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-09-07 16:13:56 +0530

    fix: bg image height

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Header } from "./components/ui/header";
 import { Navbar } from "./components/ui/navbar";
 import { Providers } from "./components/providers";
@@ -8,7 +7,7 @@ function App() {
   return (
     <Providers>
       <div className="relative h-screen w-full overflow-hidden">
-        <div className="absolute inset-0 bg-[url('/newspaper.svg')] bg-cover bg-center bg-no-repeat blur-md"></div>
+        <div className="absolute inset-0 bg-[url('/newspaper.png')] bg-cover bg-center bg-no-repeat blur-md"></div>
         <div className="relative z-10 h-full">
           <Header />
           <ComingSoon />
