66ee6bdc8a586fb770bc5029a489b101d096d571
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-09-18 11:24:40 +0530

    add: initial animation with fade

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,59 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { Header } from "./components/ui/header";
 import { Navbar } from "./components/ui/navbar";
 import { SplashScreen } from "./components/ui/splash-screen";
 import { Providers } from "./components/providers";
 
-const ANIMATION_TIMESPAN = 3000;
+const ANIMATION_TIMESPAN = 5000;
+const FADE_DURATION = 1000;
 
 function App() {
   const [showInitialAnimation, setShowInitialAnimation] = useState(true);
   const [launched, setLaunched] = useState(false);
+  const [fading, setFading] = useState(false);
+  const [showSplash, setShowSplash] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowInitialAnimation(false);
+    const animationTimer = setTimeout(() => {
+      setFading(true);
+      setTimeout(() => {
+        setShowInitialAnimation(false);
+        setShowSplash(true);
+        setTimeout(() => {
+          setFading(false);
+        }, 50); // Small delay to ensure the black screen is fully visible
+      }, FADE_DURATION);
     }, ANIMATION_TIMESPAN);
-    return () => clearTimeout(timer);
+
+    return () => clearTimeout(animationTimer);
   }, []);
 
-  if (showInitialAnimation) {
+  if (showInitialAnimation || showSplash) {
     return (
-      <div className="fixed inset-0 bg-black flex flex-col items-center justify-center text-white">
-        Initial loading animation
-      </div>
+      <>
+        {showInitialAnimation && (
+          <div className="fixed inset-0 bg-black flex flex-col items-center justify-center text-white z-10">
+            <video width={320} height={240} autoPlay>
+              <source src="/animation.mp4" type="video/mp4" />
+              Your browser does not support the video tag
+            </video>
+          </div>
+        )}
+        <div
+          className={`fixed inset-0 bg-black transition-opacity duration-1000 ${
+            fading ? "opacity-100" : "opacity-0"
+          } z-20`}
+        />
+        {showSplash && (
+          <div
+            className={`fixed inset-0 bg-black transition-opacity duration-1000 ${
+              fading ? "opacity-0" : "opacity-100"
+            } z-30`}
+          >
+            <SplashScreen setLaunched={setLaunched} />
+          </div>
+        )}
+      </>
     );
   }
 
